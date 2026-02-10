6d39cd66aa09f37e820cc5e5286fc7854cde78a6
Samuel <139263675+Sssamuellll@users.noreply.github.com>
2026-02-10 18:46:50 +0000

    Fixing Content Scaling issues
    - Fixed scaling issues with project 1 and 2.
    - Fixed positioning issues with project dropdown menu.

--- src/projects/project2/script.js
+++ src/projects/project2/script.js
@@ -1,1 +1,18 @@
 // Project 2 JavaScript Functionality:
+
+// Scaling the embedded window to an appropriate size that matches the browser window:
+function scale_content()
+{
+    const container = document.querySelector("#gameContainer");
+    const game = document.querySelector("iframe");
+
+    // Calculating the appropriate scale dependant on the game container's scale:
+    const scale = (container.offsetWidth / 960);
+    game.style.transform = `scale(${scale})`;
+    // Calculates the height of the game container based of the scale of the game:
+    container.style.height = (642 * scale) + "px";
+    //container.style.transform = `scale(${scale})`
+};
+
+window.addEventListener("resize", scale_content);
+window.addEventListener("load", scale_content);
